9bf1e57528a6e6940b2dc2faa70293dd237e6204
Bruno Harbulot <bruno@distributedmatter.net>
2013-09-24 21:37:43 +0100

    Added settings option to view as markdown for mobile devices.

--- markdown_render.js
+++ markdown_render.js
@@ -1,3 +1,8 @@
+var eejs = require('ep_etherpad-lite/node/eejs/');
+exports.eejsBlock_mySettings = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_markdown_render/templates/mySettings.ejs");
+  return cb();
+}
 
 exports.eejsBlock_styles = function (hook_name, args, cb)
 {
--- static/js/markdown_render.js
+++ static/js/markdown_render.js
@@ -1,77 +1,127 @@
 var isMobile = $.browser.mobile;
 
-if (!isMobile) {
-  var mathJaxEnabled = false;
- 
-  var documentReady = function(hook, context) {
-    $.getScript('../static/plugins/ep_markdown_render/static/marked/marked.js', function() {
-        marked.setOptions({
-          gfm: true,
-          highlight: function (code, lang, callback) {
-            try {
-              if (lang) {
-                return hljs.highlight(lang, code).value;
-              } else {
-                return hljs.highlightAuto(code).value;
-              }
-            } catch (err) {
-              return code;
+
+var mathJaxEnabled = false;
+
+var documentReady = function(hook, context) {
+  $.getScript('../static/plugins/ep_markdown_render/static/marked/marked.js', function() {
+      marked.setOptions({
+        gfm: true,
+        highlight: function (code, lang, callback) {
+          try {
+            if (lang) {
+              return hljs.highlight(lang, code).value;
+            } else {
+              return hljs.highlightAuto(code).value;
             }
-          },
-          tables: true,
-          breaks: true,
-          pedantic: false,
-          sanitize: true,
-          smartLists: true,
-          smartypants: false,
-          langPrefix: 'lang-'
-        });
-
-        $.getScript('../static/plugins/ep_markdown_render/static/highlight.js/highlight.pack.js', function() {
-          hljs.selected_languages = hljs.LANGUAGES;
-        });
-
-
-        /*
-        // MathJax Option 1 (CDN):
-        //
-        // You can use the MathJax CDN, but check the terms of service.
-        // * http://docs.mathjax.org/en/latest/start.html#mathjax-cdn
-        // * http://www.mathjax.org/download/mathjax-cdn-terms-of-service/
-        // According to some comments, the MathJax project seems to encourage
-        // the use of its CDN.
-        $.getScript('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML', function() {
-          mathJaxEnabled = true;
-        });
-        // End of MathJax Option 1
-        */
-
-        // MathJax Option 2 (Local installation):
-        //
-        // Alternatively, install a local copy in the ./static/mathjax directory.
-        $.ajax({
-          url: '../static/plugins/ep_markdown_render/static/mathjax/MathJax.js',
-          dataType: "script",
-          cache: true,
-          success: function() {
-            MathJax.Hub.config.root = '../static/plugins/ep_markdown_render/static/mathjax';
-            $.ajax({
-              url: '../static/plugins/ep_markdown_render/static/mathjax/config/TeX-AMS-MML_HTMLorMML.js',
-              dataType: "script",
-              cache: true,
-              success: function() {
-                mathJaxEnabled = true;
-              }
-            });
+          } catch (err) {
+            return code;
           }
-        });
-        // End of MathJax Option 2
+        },
+        tables: true,
+        breaks: true,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false,
+        langPrefix: 'lang-'
+      });
+
+      $.getScript('../static/plugins/ep_markdown_render/static/highlight.js/highlight.pack.js', function() {
+        hljs.selected_languages = hljs.LANGUAGES;
+      });
+
+
+      /*
+      // MathJax Option 1 (CDN):
+      //
+      // You can use the MathJax CDN, but check the terms of service.
+      // * http://docs.mathjax.org/en/latest/start.html#mathjax-cdn
+      // * http://www.mathjax.org/download/mathjax-cdn-terms-of-service/
+      // According to some comments, the MathJax project seems to encourage
+      // the use of its CDN.
+      $.getScript('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML', function() {
+        mathJaxEnabled = true;
+      });
+      // End of MathJax Option 1
+      */
+      // MathJax Option 2 (Local installation):
+      //
+      // Alternatively, install a local copy in the ./static/mathjax directory.
+      $.ajax({
+        url: '../static/plugins/ep_markdown_render/static/mathjax/MathJax.js',
+        dataType: "script",
+        cache: true,
+        success: function() {
+          MathJax.Hub.config.root = '../static/plugins/ep_markdown_render/static/mathjax';
+          $.ajax({
+            url: '../static/plugins/ep_markdown_render/static/mathjax/config/TeX-AMS-MML_HTMLorMML.js',
+            dataType: "script",
+            cache: true,
+            success: function() {
+              mathJaxEnabled = true;
+            }
+          });
+        }
+      });
+      // End of MathJax Option 2
+  });
+};
+exports.documentReady = documentReady;
+
+
+
+var renderedOnce = false;
+
+var aceEditEvent = function(hook, context) {
+  var callstack = context.callstack;
+  if (callstack
+        && (!renderedOnce || callstack.repChanged)
+        && context.editorInfo
+        && marked) {
+    try {
+        $('#renderedcontainer').html(marked(context.editorInfo.ace_exportText()));
+        renderedOnce = true;
+        if (mathJaxEnabled) {
+          var math = document.getElementById("renderedcontainer");
+          MathJax.Hub.Queue(["Typeset",MathJax.Hub,math]);
+        }
+    } catch (err) {
+      console.log("Error when rendering the Markdown markup.", err);
+    }
+  }
+};
+exports.aceEditEvent = aceEditEvent;
+
+
+if (isMobile) {
+  var postAceInit = function(hook, context){
+    $('#editorcontainer').before($('<div id="renderedcontainer"></div>'));
+    $('#renderedcontainer').css({
+      position: 'absolute',
+      top: $('#editorcontainer').css('top'),
+      right: '5px',
+      left: '5px',
+      bottom: $('#editorcontainer').css('bottom'),
+      "z-index": 1
+    });
+    $('#editorcontainer').hide();
+    renderedOnce = false;
+    pad.changeViewOption('useMonospaceFont', true);
+    $('#editbar .menu_left').hide();
+
+    $('#view-as-markdown').on('click', function() {
+      if ($('#view-as-markdown').prop('checked')) {
+        $('#editorcontainer').show();
+        $('#renderedcontainer').hide();
+      } else {
+        $('#editorcontainer').hide();
+        $('#renderedcontainer').show();
+      }
     });
   };
-  exports.documentReady = documentReady;
-
-  var renderedOnce = false;
-
+  exports.postAceInit = postAceInit;
+} else {
   var postAceInit = function(hook, context){
     $('#editorcontainer').before($('<div id="renderedcontainer"></div>'));
     $('#editorcontainer').css({ left: '50%' });
@@ -86,27 +136,7 @@ if (!isMobile) {
     renderedOnce = false;
     pad.changeViewOption('useMonospaceFont', true);
     $('#editbar .menu_left').hide();
+    $('#view-as-markdown-block').hide();
   };
   exports.postAceInit = postAceInit;
-
-  var aceEditEvent = function(hook, context) {
-    var callstack = context.callstack;
-    if (callstack
-          && (!renderedOnce || callstack.repChanged)
-          && context.editorInfo
-          && marked) {
-      try {
-        $('#renderedcontainer').html(marked(context.editorInfo.ace_exportText()));
-        renderedOnce = true;
-        if (mathJaxEnabled) {
-          var math = document.getElementById("renderedcontainer");
-          MathJax.Hub.Queue(["Typeset",MathJax.Hub,math]);
-        }
-      } catch (err) {
-        console.log("Error when rendering the Markdown markup.", err);
-      }
-    }
-  };
-  exports.aceEditEvent = aceEditEvent;
 }
-
